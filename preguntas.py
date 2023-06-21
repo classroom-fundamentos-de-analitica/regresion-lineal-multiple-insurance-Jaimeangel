ceffc069d7570979efe410c69d77a7892ef7c8c3
Jaime A <jaimeangel1097@gmail.com>
2023-06-21 09:34:30 -0500

    corrigiendo pregunta 02

--- preguntas.py
+++ preguntas.py
@@ -47,7 +47,7 @@ def pregunta_02():
 
     # Divida los datos de entrenamiento y prueba. La semilla del generador de números
     # aleatorios es 12345. Use 300 patrones para la muestra de prueba.
-    (X_train, X_test, y_train, y_test,) = ____(
+    (X_train, X_test, y_train, y_test,) = train_test_split(
         X,
         y,
         test_size=300,
